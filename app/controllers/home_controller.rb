6a94fa3ef18a79b10a4485497787c1c3b533e427
EC2 Default User <ec2-user@ip-172-31-16-220.ec2.internal>
2021-12-13 17:54:15 +0000

    bookの新規登録機能

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,4 @@
 class HomeController < ApplicationController
   def index
   end
-  
-  def create
-    redirect_to book_path(book.id) 
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
 end
